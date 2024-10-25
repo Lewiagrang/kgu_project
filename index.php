5fc19b9af82ed6da502fab9e724da0de83b51b77
Lewiagrang <166782741+Lewiagrang@users.noreply.github.com>
2024-10-25 20:15:26 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+// Подключение файла соединения с БД
+include_once 'db.class.php';
+
+// хост БД
+define('db_host','localhost');
+
+// Имя БД
+define('db_name','shop');
+
+// Пользователь БД
+define('db_user','admin');
+
+// Пароль БД
+define('db_pass','admin_password');
+
